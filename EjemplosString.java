6627c02f2b4ccba9f2fda17e388aab340e76f61f
jmhr0 <jherram0603@g.educaand.es>
2022-10-21 12:28:06 +0200

    Create EjemplosString.java
    Primer commit

---
+++ EjemplosString.java
@@ -0,0 +1,16 @@
+public class EjemplosString {
+    public static void main(String[] args) {
+    //crear variable string
+    String nombre = "Juanma";
+    String apellido = "Herrera";
+    float edad = 18;
+    //imprimir el valor de la variable
+    //System.out.println("Hola " + s);
+    float z = 2/3;
+    
+    //imprimir con formato
+    //Hola Fran Gomez, tengo 18 años
+    System.out.printf("Hola %s %s, tengo: %.4f \t \"siguiente\" linea \n" , nombre,apellido,edad);
+    System.out.printf("Hola %s ", z);
+    }
+}
