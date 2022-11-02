8af3501c16925f69fcd0fb4bc033be855cf230ac
jmhr0 <jherram0603@g.educaand.es>
2022-11-02 08:36:24 +0100

    Clase 02/11/2022
    Ejercicios propuestos en clase

---
+++ Enum.java
@@ -0,0 +1,15 @@
+public class Enum {
+    enum Dia {
+        LUNES,
+        MARTES,
+        MIERCOLES,
+        JUEVES,
+        VIERNES,
+        SABADO,
+        DOMINGO
+    }
+public static void main (String [] Args){
+
+
+    }
+} 
---
+++ Parimpar.java
@@ -0,0 +1,6 @@
+public class Parimpar {
+    public static void main (String [] args){
+        Double aleatorio = Math.random()*1000;
+        System.out.println(aleatorio);
+    }
+}
