e05cfc946052fc15214d46454f0272dc91023b2b
jmhr0 <jherram0603@g.educaand.es>
2022-11-02 08:14:46 +0100

    Ejercicio switch para meses

---
+++ EjercicioSwitchmeses.java
@@ -0,0 +1,56 @@
+public class EjercicioSwitchmeses{
+public static void main(String[] args) {
+//dado un mes que diga el numero de dias que tiene
+Integer mes = 11;
+switch(mes){
+    case 0:
+    System.out.println("Enero");
+    System.out.println("Tiene 31 días");
+    break;
+    case 1:
+    System.out.println("Febrero");
+    System.out.println("Tiene 28-29 dias");
+    break;
+    case 2:
+    System.out.println("Marzo");
+    System.out.println("Tiene 31 dias");
+    break;
+    case 3:
+    System.out.println("Abril");
+    System.out.println("Tiene 30 dias");
+    break;
+    case 4:
+    System.out.println("Mayo");
+    System.out.println("Tiene 31 dias");
+    break;
+    case 5:
+    System.out.println("Junio");
+    System.out.println("Tiene 30 dias");
+    break;
+    case 6:
+    System.out.println("Julio");
+    System.out.println("Tiene 31 días");
+    break;
+    case 7:
+    System.out.println("Agosto");
+    System.out.println("Tiene 31 dias");
+    break;
+    case 8:
+    System.out.println("Septiembre");
+    System.out.println("Tiene 30 dias");
+    break;
+    case 9:
+    System.out.println("Octubre");
+    System.out.println("Tiene 31 dias");
+    break;
+    case 10:
+    System.out.println("Noviembre");
+    System.out.println("Tiene 30 dias");
+    break;
+    case 11:
+    System.out.println("Diciembre");
+    System.out.println("Tiene 31 dias");
+    break;
+}
+}
+}
